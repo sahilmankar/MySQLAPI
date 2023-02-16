e45fcee14c0a500d31e90f294e4795bd4e68cb46
sahil mankar <sahilmankar311@gmail.com>
2023-02-16 21:39:18 +0530

    files updated

--- router/router.js
+++ router/router.js
@@ -2,23 +2,23 @@ const express = require("express");
 const router = express.Router();
 const productscontroller = require("../controllers/productscontroller");
 
-router.get("/api/getall", (request, response) => {
+router.get("/api/products", (request, response) => {
   productscontroller.GetAllProducts(request, response);
 });
 
-router.get("/api/getall/:id", (request, response) => {
+router.get("/api/products/:id", (request, response) => {
   productscontroller.GetById(request, response);
 });
 
-router.post("/api/getall", (request, response) => {
+router.post("/api/products/insert", (request, response) => {
   productscontroller.InsertProduct(request, response);
 });
 
-router.put("/api/getall/:id", (request, response) => {
+router.put("/api/products/update/:id", (request, response) => {
   productscontroller.UpdateProduct(request, response);
 });
 
-router.delete("/api/getall/:id", (request, response) => {
+router.delete("/api/products/delete/:id", (request, response) => {
   productscontroller.DeleteById(request, response);
 });
 
